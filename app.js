e61b6d7c9216df8a6c1682221cb40c55a9e17b7e
waltercarvajalc <waltercarvajalc@gmail.com>
2022-06-19 13:16:34 -0500

    Se agrega el pryecto

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
+const usuarios = require('./routes/usuario');
+const tiposEquipo = require('./routes/tipoEquipo');
+const estados = require('./routes/estado');
+const marcas = require('./routes/marca');
+
+const inventarios = require('./routes/inventario');
+
+//middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(fileUpload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/'
+}));
+app.use(cors());
+
+
+app.use('/api/usuarios', usuarios);
+app.use('/api/tiposequipo', tiposEquipo);
+app.use('/api/estados', estados);
+app.use('/api/marcas', marcas);
+app.use('/api/inventarios', inventarios);
+
+module.exports = app;
